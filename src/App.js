1d24fb62a431f561305af812782d25e5041f550b
Piervincenzo Parisi <piervincenzo.parisi@nexse.com>
2018-03-14 17:57:12 +0100

    first commit of the react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'tachyons';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title f1">Speech Recognition</h1>
+        </header>
+        <section className="App-content">
+          <p>App content goes here</p>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
