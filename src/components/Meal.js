5b4386f44baca151c5f6572920943deb0e318945
Piervincenzo Parisi <piervi_parisi@yahoo.it>
2018-03-21 14:30:44 +0100

    style adjustments

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './Meal.css'
+// import './Meal.css'
 
 const Meal = ({name, id}) => {
     return (
         <div>
             <div className="fl w-80 pa2 bb b--near-white">{name}</div>
-            <div className="fl w-20 pa2 bb b--near-white"><input value="0"></input></div>
+            <div className="fl w-20 pa2 bb b--near-white">0</div>
         </div>
     )
 }
